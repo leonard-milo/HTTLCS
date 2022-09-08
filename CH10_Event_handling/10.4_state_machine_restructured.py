35bf9b545f4e1e1f07e571eba38bd10c57929fbf
Leonard Chi <leonardochi168@gmail.com>
2022-09-08 16:08:58 +0100

    10.4 state machines (restructured)

---
+++ CH10_Event_handling/10.4_state_machine_restructured.py
@@ -0,0 +1,62 @@
+import turtle
+
+def setup_trutle():
+    global tess, wn
+    turtle.setup(400,500)
+    wn = turtle.Screen()
+    wn.title("Tess becomes a traffic light!")
+    wn.bgcolor("lightgreen")
+    tess = turtle.Turtle()
+
+def draw_housing():
+    """ Draw a nice housing to hold the traffic lights """
+    tess.pensize(3)
+    tess.color("black", "darkgrey")
+    tess.begin_fill()
+    tess.forward(80)
+    tess.left(90)
+    tess.forward(200)
+    tess.circle(40, 180)
+    tess.forward(200)
+    tess.left(90)
+    tess.end_fill()
+    tess.penup()
+
+def draw_greenlight():
+    # Position tess onto the place where the green light should be
+    tess.forward(40)
+    tess.left(90)
+    tess.forward(50)
+    # Turn tess into a big green circle
+    tess.shape("circle")
+    tess.shapesize(3)
+    tess.fillcolor("green")
+
+def advance_state_machine():
+    global state_num
+    if state_num == 0:       # Transition from state 0 to state 1
+        tess.forward(70)
+        tess.fillcolor("orange")
+        state_num = 1
+    elif state_num == 1:     # Transition from state 1 to state 2
+        tess.forward(70)
+        tess.fillcolor("red")
+        state_num = 2
+    else:                    # Transition from state 2 to state 0
+        tess.back(140)
+        tess.fillcolor("green")
+        state_num = 0
+
+def main():
+    setup_trutle()
+    draw_housing()
+    draw_greenlight()
+    global state_num
+    state_num = 0
+    wn.onkey(advance_state_machine, "space")
+
+    wn.listen()                      # Listen for events
+    wn.mainloop()
+
+if __name__ == "__main__":
+    main()
