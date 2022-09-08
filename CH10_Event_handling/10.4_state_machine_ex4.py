0c6da203720d3c50b7d0abf86e5f78ef6774d059
Leonard Chi <leonardochi168@gmail.com>
2022-09-08 17:06:21 +0100

    10.4 state machine ex4

---
+++ CH10_Event_handling/10.4_state_machine_ex4.py
@@ -0,0 +1,104 @@
+import turtle
+
+def setup_window():
+    global wn
+    turtle.setup(400,500)
+    wn = turtle.Screen()
+    wn.title("Tess becomes a traffic light!")
+    wn.bgcolor("lightgreen")
+
+def draw_housing():
+    global house
+    house = turtle.Turtle()
+    """ Draw a nice housing to hold the traffic lights """
+    house.pensize(3)
+    house.color("black", "darkgrey")
+    house.begin_fill()
+    house.forward(80)
+    house.left(90)
+    house.forward(200)
+    house.circle(40, 180)
+    house.forward(200)
+    house.left(90)
+    house.end_fill()
+    house.penup()
+
+def draw_orlight():
+    orl = turtle.Turtle()
+
+def draw_greenlight():
+    global gturtle
+    gturtle = turtle.Turtle()
+    # Position tess onto the place where the green light should be
+    gturtle.penup()
+    gturtle.forward(40)
+    gturtle.left(90)
+    gturtle.forward(50)
+    # Turn tess into a big green circle
+    gturtle.shape("circle")
+    gturtle.shapesize(3)
+    gturtle.fillcolor("green")
+
+def draw_orglight():
+    global orgturtle
+    orgturtle = turtle.Turtle()
+    # Position tess onto the place where the green light should be
+    orgturtle.penup()
+    orgturtle.forward(40)
+    orgturtle.left(90)
+    orgturtle.forward(50)
+    orgturtle.forward(70)
+    # Turn tess into a big green circle
+    orgturtle.shape("circle")
+    orgturtle.shapesize(3)
+    orgturtle.fillcolor("orange")
+
+def draw_redlight():
+    global redturtle
+    redturtle = turtle.Turtle()
+    # Position tess onto the place where the green light should be
+    redturtle.penup()
+    redturtle.forward(40)
+    redturtle.left(90)
+    redturtle.forward(50)
+    redturtle.forward(70)
+    redturtle.forward(70)
+    # Turn tess into a big green circle
+    redturtle.shape("circle")
+    redturtle.shapesize(3)
+    redturtle.fillcolor("red")
+
+def advance_state_machine():
+    global state_num
+    if state_num == 0:       # Transition from state 0 to state 1
+        redturtle.fillcolor(q4light[state_num][0])
+        orgturtle.fillcolor(q4light[state_num][1])
+        gturtle.fillcolor(q4light[state_num][2])
+        state_num = 1
+    elif state_num == 1:     # Transition from state 1 to state 2
+        redturtle.fillcolor(q4light[state_num][0])
+        orgturtle.fillcolor(q4light[state_num][1])
+        gturtle.fillcolor(q4light[state_num][2])
+        state_num = 2
+    else:                    # Transition from state 2 to state 0
+        redturtle.fillcolor(q4light[state_num][0])
+        orgturtle.fillcolor(q4light[state_num][1])
+        gturtle.fillcolor(q4light[state_num][2])
+        state_num = 0
+
+def main():
+    setup_window()
+    draw_housing()
+    draw_greenlight()
+    draw_orglight()
+    draw_redlight()
+    global state_num, q4light
+    state_num = 0
+    q4light = [["black", "black", "green"],["black", "orange", "black"],["red", "black", "black"]]
+    wn.onkey(advance_state_machine, "space")
+
+    wn.listen()                      # Listen for events
+    wn.mainloop()
+
+if __name__ == "__main__":
+    main()
